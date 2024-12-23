fcd9c12960c66cd09cc946ce682a16a719aaa814
Sergiu333 <anghelenicis59@gmail.com>
2024-12-23 21:30:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const params1 = {
     dataUrl: 'http://46.97.226.95:8005/doc/index.html#/intelligentDisplay',
     punctDeTrecere: '66eceab9-6b2a-4266-895f-1babcdf3023f',
     loginData: loginData1,
+    idAgent : 3,
 };
 
 const params2 = {
-    loginUrl: 'http://92.114.165.156:84/doc/index.html#/portal/login',
-    dataUrl: 'http://92.114.165.156:84/doc/index.html#/intelligentDisplay',
+    loginUrl: 'http://92.114.165.156:8002/doc/index.html#/portal/login',
+    dataUrl: 'http://92.114.165.156:8002/doc/index.html#/intelligentDisplay',
     punctDeTrecere: '8aa3f7d9-8437-4214-bc3e-51fc8d425984',
     loginData: loginData1,
+    idAgent : 2,
 };
 
 
